e02a09415e86124046dc19d8e8c7890ee4015254
EGRazo <59321368+EGRazo@users.noreply.github.com>
2021-12-29 00:46:59 -0800

    Create Multiplier.v

---
+++ Multiplier.v
@@ -0,0 +1,14 @@
+module Multiplier #(parameter WL = 32)
+(input signed [WL - 1 : 0] xin1 ,
+	input signed [WL - 1 : 0] xin2,
+	output signed reg [WL - 1 : 0] out);
+
+initial begin
+out <= 0;
+end 
+
+always @(xin1, xin2) begin
+	out <= xin1 * xin2;
+end
+
+endmodule
